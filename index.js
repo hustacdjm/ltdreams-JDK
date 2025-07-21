b3c43e75460233c61901dd1356d49e58c70e8476
hustacdjm <hustacdjm@gmail.com>
2025-07-21 13:24:06 -0700

    add connection

--- index.js
+++ index.js
@@ -4,26 +4,22 @@ export class LTDreamsClient {
 
     constructor() {
       this.apiBaseUrl = null;
+      this.token = null;
+      this.headers=null;
     }
   
     /**
      * Connects to the domain and retrieves the base API URL.
      * @param {string} domain - The domain (e.g., "ltdreams.com")
+     * @param env.Authorized ->using different token
      */
-     connect(domain) {
-        return new Promise((resolve, reject) => {
-          try {
-            this.apiBaseUrl = domain;
+     connect(domain, header) {
+        if (!domain) {
+          throw new Error('api_base_url not found in response');
+        }
       
-            if (!this.apiBaseUrl) {
-              throw new Error('api_base_url not found in response');
-            }
-      
-            resolve(this.apiBaseUrl);
-          } catch (err) {
-            reject(new Error(`Failed to connect to domain ${domain}: ${err.message}`));
-          }
-        });
+        this.apiBaseUrl = domain;
+        this.header = header;
       }
   
     /**
@@ -39,7 +35,7 @@ export class LTDreamsClient {
         const url = `${this.apiBaseUrl}/apiCallByRequest/${encodeURIComponent(apiId)}`;
       
         return axios
-          .post(url, request) // use POST if sending a body
+          .post(url, request, this.headers) // use POST if sending a body
           .then(res => res.data)
           .catch(err => {
             throw new Error(`Failed to call /callApi: ${err.message}`);
